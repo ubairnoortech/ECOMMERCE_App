177a07d4acc7ada72451dc2f5d64bb74fc2d162a
unknown <ubair.noor@live.iium.edu.my>
2023-12-07 18:28:09 +0800

    Database Added

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 require("dotenv").config()
 
 app.get("/", (req,res) => {
-    res.send("hello from node")
+    res.send("from nodemone updated")
 });
 
 const port = process.env.PORT || 8000
@@ -14,4 +14,17 @@ const port = process.env.PORT || 8000
 
 app.listen(port, () =>{
     console.log(`Server is running on port ${port}`)
-})
+});
+
+const mongoose = require('mongoose');
+
+const dotenv =  require('dotenv')
+dotenv.config()
+
+// DB
+mongoose.connect(
+    process.env.DATABASE,{
+        useNewUrlParser: true,
+        useCreateIndex: true
+    }
+). then(() =>console.log('DB Connected'))
